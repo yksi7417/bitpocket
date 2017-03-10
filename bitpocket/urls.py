3b3f965b1b8206d75af49b11f81dd6a10ad45103
yksi7417 <yatkasi@gmail.com>
2017-03-10 22:12:41 +0800

    with css javascript etc

--- bitpocket/urls.py
+++ bitpocket/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'', include('login.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
