525d38309ba377a1fd09f3dcbe5a5f0d49390428
yksi7417 <yatkasi@gmail.com>
2017-03-10 23:36:40 +0800

    data model to keep track of account_balance

--- bitpocket/views.py
+++ bitpocket/views.py
@@ -2,6 +2,6 @@ from django.views.generic.base import View
 from django.http import JsonResponse
 
 class PlayView(View):
-    def dispatch(request, *args, **kwargs):
-        print(request)
-        return JsonResponse({'body': { 'accountBalance': '1000'} })
+    def dispatch(self, *args, **kwargs):
+        profile = self.request.user.profile
+        return JsonResponse({'body': { 'accountBalance': profile.account_balance } })
