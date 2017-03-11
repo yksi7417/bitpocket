337534f3c9f14ee3a1f9ce43fcc7da252b127229
yksi7417 <yatkasi@gmail.com>
2017-03-11 09:16:28 +0800

    7 levels, json working

--- bitpocket/views.py
+++ bitpocket/views.py
@@ -1,7 +1,9 @@
 from django.views.generic.base import View
 from django.http import JsonResponse
+import json
 
 class PlayView(View):
     def dispatch(self, *args, **kwargs):
+        json_data = json.loads(self.request.body.decode('utf-8'))
         profile = self.request.user.profile
-        return JsonResponse({'body': { 'accountBalance': profile.account_balance } })
+        return JsonResponse({'body': { 'accountBalance': profile.account_balance + json_data['guess']} })
