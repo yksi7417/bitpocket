24911f06e315ed34b3c7e9c6778104f3ffde5bce
yksi7417 <yatkasi@gmail.com>
2017-03-11 10:00:18 +0800

    javascript to enable disable button

--- bitpocket/views.py
+++ bitpocket/views.py
@@ -6,4 +6,16 @@ class PlayView(View):
     def dispatch(self, *args, **kwargs):
         json_data = json.loads(self.request.body.decode('utf-8'))
         profile = self.request.user.profile
-        return JsonResponse({'body': { 'accountBalance': profile.account_balance + 10 * json_data['level'] + json_data['guess']} })
+        return JsonResponse({'body': {
+            'accountBalance': profile.account_balance + 10 * json_data['level'] + json_data['guess'],
+            'currentLevel' : 4,
+            'result' : {
+                0: {0: 'W', 1: 'L'},
+                1: {0: 'W', 1: 'L'},
+                2: {0: 'W', 1: 'L'},
+                3: {0: 'W', 1: 'L'},
+                4: {0: 'W', 1: 'L'},
+                5: {0: 'W', 1: 'L'},
+                6: {0: 'W', 1: 'L'},
+            }
+        } })
