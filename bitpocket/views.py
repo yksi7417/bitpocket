c2b518c895f67b414b66bcf544b9b1988a869daa
yksi7417 <yatkasi@gmail.com>
2017-03-11 09:17:59 +0800

    confirm i can get levels from json

--- bitpocket/views.py
+++ bitpocket/views.py
@@ -6,4 +6,4 @@ class PlayView(View):
     def dispatch(self, *args, **kwargs):
         json_data = json.loads(self.request.body.decode('utf-8'))
         profile = self.request.user.profile
-        return JsonResponse({'body': { 'accountBalance': profile.account_balance + json_data['guess']} })
+        return JsonResponse({'body': { 'accountBalance': profile.account_balance + 10 * json_data['level'] + json_data['guess']} })
