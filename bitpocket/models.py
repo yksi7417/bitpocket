ed8b11cab3ced9e65f8ae62edba313061e06e198
yksi7417 <yatkasi@gmail.com>
2017-03-15 17:23:24 +0800

    basic interaction with persisted game result

--- bitpocket/models.py
+++ bitpocket/models.py
@@ -2,10 +2,29 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import json
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     account_balance = models.IntegerField(default=0)
+    current_level = models.IntegerField(default=0)
+    num_of_games_played = models.IntegerField(default=0)
+    game_result = models.TextField(blank=True)
+
+    def reset_game_result(self):
+        self.game_result = ''
+        self.current_level = 0
+
+
+    def append_line_result(self, line_number, line_result):
+        if (self.game_result != ''):
+            game_result_dict = json.loads(self.game_result)
+        else:
+            game_result_dict = {}
+        game_result_dict[str(line_number)] = {}
+        game_result_dict[str(line_number)]['0'] = line_result[0]
+        game_result_dict[str(line_number)]['1'] = line_result[1]
+        self.game_result = json.dumps(game_result_dict)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
--- bitpocket/views.py
+++ bitpocket/views.py
@@ -2,14 +2,71 @@ from django.views.generic.base import View
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
 import json
+import random
 
 from django.core import serializers
 
 class PlayView(View):
+
+    def __flip(self, guess):
+        if guess == 0:
+            return 1
+        return 0
+
+    def __resetGame(self, profile):
+        profile.reset_game_result()
+        profile.save()
+
+    def __handleWin(self, guess, profile):
+        profile.account_balance += 10
+        line_result = self.__processLineResult(guess, 'W', 'L')
+        profile.append_line_result(profile.current_level, line_result)
+        profile.current_level += 1
+        profile.save()
+
+    def __processLineResult(self, guess, first, second):
+        line_result = ['?', '?']
+        line_result[guess] = first
+        line_result[self.__flip(guess)] = second
+        return line_result
+
+    def __handleLoss(self, guess, profile):
+        profile.account_balance -= 10
+        line_result = self.__processLineResult(guess, 'L', 'W')
+        profile.append_line_result(profile.current_level, line_result)
+        profile.current_level += 1
+        profile.save()
+
+    def __getResultFromVerifiableSource(self):
+        ##  this is not verifiable yet!    mock with random.math for now.
+        randomNum = random.choice([0,1])
+        if (randomNum < 0.5):
+            return 0
+        else:
+            return 1
+
+    def makeOneGuess(self, profile, guess) :
+        result = self.__getResultFromVerifiableSource()
+        if (guess == result):
+            self.__handleWin(guess, profile)
+        else:
+            self.__handleLoss(guess, profile)
+
     def dispatch(self, *args, **kwargs):
         json_data = json.loads(self.request.body.decode('utf-8'))
         profile = self.request.user.profile
+
+        if (json_data['guess'] == -1):
+            self.__resetGame(profile)
+        else:
+            self.makeOneGuess(profile, json_data['guess'])
+
         dict = { 'body' : model_to_dict(profile) }
+        if (dict['body']['game_result'] == ''):
+            dict['body']['game_result'] = {}
+        else:
+            dict['body']['game_result'] = json.loads(dict['body']['game_result'] )
+
         return JsonResponse(dict)
 
 
